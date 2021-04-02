e2a3c0fdd47fcb693e3a1924cedc18135f001b07
M1sterGlass <56590900+M1sterGlass@users.noreply.github.com>
2021-04-02 11:43:03 +0200

    Update Install-LinuxMint.sh

--- Install-LinuxMint.sh
+++ Install-LinuxMint.sh
@@ -1,8 +1,8 @@
 # LINUX MINT
 
 sudo apt update
-sudo apt -y upgrade 
-sudo apt -y dist-upgrade
+sudo apt upgrade -y 
+sudo apt dist-upgrade -y
 sudo apt autoremove
 sudo apt autoclean
 
@@ -10,24 +10,16 @@ df
 free -h
 swapon
 
-# REMOVE
-
-sudo apt -y --purge autoremove thunderbird  thunderbird-gnome-support 
-
 # NETWORKTOOLS
 
-read -r -p "Install networktools? [Y/N] " networktools
-case $networktools in
-[yY])
+read -n 1 -p "Install networktools? [y/n] " yn
+case $yn
+    [Yy] )
     sudo apt install -y traceroute
     sudo apt install -y nmap
     sudo apt install -y iftop
     sudo apt install -y wireshark
-;;
-*)
-    echo "Invalid input"
-    exit 1
-;;
+    ;;
 esac
 
 # FIREWALL
@@ -40,7 +32,9 @@ sudo ufw status verbose
 # sudo ufw allow 80,443,25,587,465,143,993/tcp
 # cat /var/log/ufw.log
 
+# REMOVE
 
+sudo apt -y --purge autoremove thunderbird  thunderbird-gnome-support 
 
 # CHROME
 
@@ -51,14 +45,14 @@ sudo apt install -y google-chrome-stable
 
 # VSCODE
 
-sudo apt -y install wget apt-transport-https
+sudo apt install -y wget apt-transport-https
 sudo apt update
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 cat  /etc/apt/sources.list.d/vscode.list
 sudo apt update 
-sudo apt -y install code
+sudo apt install -y code
 
 # VLC
 
@@ -67,18 +61,5 @@ sudo apt install -y vlc
 # GIMP
 
 sudo apt install -y gimp
-
-# REMOVE DUPLICATE
-sudo -s
-FILE="/etc/apt/sources.list.d/additional-repositories.list"
-ls -la $FILE
-mv $FILE $FILE.bak
-sort $FILE.bak | uniq -u > $FILE
-
-FILE="/etc/apt/sources.list.d/google-chrome.list"
-ls -la $FILE
-mv $FILE $FILE.bak
-sort $FILE.bak | uniq -u > $FILE
-exit
 
 
