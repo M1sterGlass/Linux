38ee7a9909ce60e434eec154f6b969f37116a27c
M1sterGlass <56590900+M1sterGlass@users.noreply.github.com>
2021-11-30 19:45:54 +0100

    Update Install-LinuxKali.sh

--- Install-LinuxKali.sh
+++ Install-LinuxKali.sh
@@ -1,2 +1,48 @@
-sudo apt update
-sudo apt upgrade
+ SYTEMINFO
+
+df
+free -h
+swapon
+
+# UPDATES
+
+read -p "Install updates? [y/n] " yn
+case $yn in
+    [Yy] )
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt full-upgrade -y
+    sudo apt dist-upgrade -y
+    sudo apt autoremove
+    sudo apt autoclean
+    ;;
+esac
+
+
+read -p "Enable virtualbox USB passtrough? [y/n] " yn
+case $yn in
+    [Yy] )
+    echo "Run on host machine:"
+    echo "####################"
+    echo "sudo adduser $USER vboxusers"
+    echo "sudo usermod -a -G vboxusers $USER" 
+    ;;
+esac
+
+# WIFITE
+
+read -p "Install wifite? [y/n] " yn
+case $yn in
+    [Yy] )
+
+    sudo apt-get install libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev -y
+    sudo apt install hcxtools -y
+
+    git clone https://github.com/JPaulMora/Pyrit.git
+    sudo apt-get install python3 python3-pip libpcap-dev python2-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev -y
+    cd Pyrit
+    sudo python setup.py clean
+    sudo python setup.py build   
+    sudo python setup.py install
+    ;;
+esac
